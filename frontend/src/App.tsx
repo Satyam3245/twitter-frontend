8b2b4fab95f8176c9a19de82d3c0c0a709e7ab9c
Satyam <mitravlakshay@gmail.com>
2024-10-02 15:04:39 +0530

    add_some =_fun

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import SearchBar from "./custom-components/searchbar"
+import Show from "./custom-components/show"
 
 function App() {
   return <div>
     <SearchBar/>
+    <Show/>
   </div>
 }
 export default App
